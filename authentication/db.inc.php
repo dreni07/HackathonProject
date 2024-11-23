965e7f3db4c279d94d76f9f924b0844437fb8071
dreni07 <drenllazani10@gmail.com>
2024-11-23 10:40:30 +0100

    ...

---
+++ authentication/db.inc.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'mysql:host=localhost;dbname=hackathon';
+    $username = 'root';
+    $password = '';
+
+    try{
+        $pdo = new PDO($host,$username,$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+        die('Failed Because Of ' . $e->getMessage());
+    }
+?>
